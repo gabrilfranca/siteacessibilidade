499c91bdefec3e90eb52fae762b9b0710cd88067
gabrilfranca <164918506+gabrilfranca@users.noreply.github.com>
2025-03-26 14:41:42 -0300

    g

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-        document.addEventListener('DOMContentLoaded', fduntion(){
-            let tamanhoAtualFonte=1;
-            const aumentaFonteBotao= document.getAnimations('aumentar-fonte');
-            
-                ')
-        }
+document.addEventListener('DOMContentLoaded', function(){
+    let tamanhoAtualFonte= 1;
+    const aumentaFonteBotao= document.getElementById('aumentar-fonte');
+   
+    aumentaFonteBotao.addEventListener('click', function(){
+       tamanhoAtualFonte += 0.1;
+       document.body.style.fontSize= '${tamanhoAtualFonte}ren';
+    }
+    }
